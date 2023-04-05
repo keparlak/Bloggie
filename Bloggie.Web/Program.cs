c7315c802c5d8fe45629d57f2d8feb1576593b52
Kadir Emre Parlak <kemreparlak@gmail.com>
2023-04-05 13:17:38 +0300

    BlogPost models Created.

--- Bloggie.Web/Program.cs
+++ Bloggie.Web/Program.cs
@@ -1,5 +1,6 @@
 namespace Bloggie.Web
 {
+    //program.cs dostasý uygulamanýn konfigürasyon ayarlarýnýn yaðýldýðý uygulamanýn baþladýðý dosyadýr. Builder adýnda bir middleware'imiz bulunmaktadýr. Bu middleware ile programda kullanacaðýz tüm servisleri tanýmlamaktayýz. Ýleride kullanacaðýmýz tüm ek servisleri bu alanda tanýmlayacaðýz.
     public class Program
     {
         public static void Main(string[] args)
@@ -12,6 +13,9 @@ namespace Bloggie.Web
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+
+            //app. diyerek kullandýðýmýz request sürecindeki tüm özellikler aþaðýda tanýmlanmýþtýr. Ýþimize yarayacak olan özelliklerin tamamýný da app. diyerek  tanýmlýyor olacaðýz. Örneðin aþaðýda app.UseAuthorization() özelliði request pipeline'a tanýmlanmýþtýr. Bizler ise kimlik kontrolü yapmak istediðimizde Authentication özelliðini kullanýyor olacaðýz. Bunun için ileride app.UseAuthentication() isimli özelliði dahil edeceðiz.
+
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
@@ -25,6 +29,8 @@ namespace Bloggie.Web
             app.UseRouting();
 
             app.UseAuthorization();
+
+            //http://localhost:5050/Kitap/Index/
 
             app.MapControllerRoute(
                 name: "default",
