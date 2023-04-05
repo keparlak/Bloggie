2d3edf5984bf74a45ae5364c5fd69abf43b092ba
Kadir Emre Parlak <kemreparlak@gmail.com>
2023-04-05 15:01:21 +0300

    AdminTags controller created. Action added inside to AdminTags. Created view from AddActions.

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        //Yapmış olduğumuz MVC routing şekli yandaki gibi olacaktır: http://localhost:1544/AdminTags/Add
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
