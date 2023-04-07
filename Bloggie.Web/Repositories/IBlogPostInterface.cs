13f40beb61027760fc48da485813a838834e30dd
Kadir Emre Parlak <kemreparlak@gmail.com>
2023-04-07 18:07:14 +0300

    AdminBlogPosts CRUD operations created.

---
+++ Bloggie.Web/Repositories/IBlogPostInterface.cs
@@ -0,0 +1,13 @@
+﻿using Bloggie.Web.Models.Domain;
+
+namespace Bloggie.Web.Repositories
+{
+	public interface IBlogPostInterface
+	{
+		Task<IEnumerable<Tag>> GetAllAsync();
+		Task<BlogPost?> GetAsync(Guid id);
+		Task<BlogPost?> AddAsync(Tag tag);
+		Task<BlogPost?> UpdateAsync(Tag tag);
+		Task<BlogPost?> DeleteAsync(Guid id);
+	}
+}
