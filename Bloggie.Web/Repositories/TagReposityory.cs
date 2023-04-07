bebcc7e3430f3c16c2c965f08cf9da825e5fa0a3
Kadir Emre Parlak <kemreparlak@gmail.com>
2023-04-07 13:05:57 +0300

    TagRepository and TagInterface created. The methods have been defined. The bodies have begun to be filled.

---
+++ Bloggie.Web/Repositories/TagReposityory.cs
@@ -0,0 +1,41 @@
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+
+namespace Bloggie.Web.Repositories
+{
+    public class TagReposityory : ITagInterface
+    {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        public TagReposityory(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+        public async Task<Tag?> AddAsync(Tag tag)
+        {
+            await bloggieDbContext.Tags.AddAsync(tag);
+            await bloggieDbContext.SaveChangesAsync();
+            return tag;
+        }
+
+        public Task<Tag> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Tag>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tag> GetASync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tag> UpdateAsync(Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
